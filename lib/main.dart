434ee70c249d167ba3a6319faa528295847a12e4
rizqyfahmi <rizqyfahmi@yahoo.com>
2022-03-23 18:19:06 +0700

    Draggable, DragTarget, and SizedBox

--- lib/main.dart
+++ lib/main.dart
@@ -4,73 +4,125 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class Data {
+    final Color color;
+    final String message;
+
+    Data({this.color = Colors.black12, this.message = ""});
+}
+
+class MyApp extends StatefulWidget {
   const MyApp({ Key? key }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-                title: const Text("Hello world application"),
-            ),
-            body: Container(
-                width: double.infinity,
-                color: Colors.amber,
-                child: Column(
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+    Color? targetColor;
+    
+    @override
+    Widget build(BuildContext context) {
+        return MaterialApp(
+            home: Scaffold(
+                appBar: AppBar(
+                    title: const Text("Hello world application"),
+                ),
+                body: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
                     children: [
                         Flexible(
                             flex: 1,
                             fit: FlexFit.tight,
-                            child: Container(
-                                color: Colors.green,
-                                child: Row(
-                                    children: const [
-                                        // Spacer is a widget used to add space that fills remain space
-                                        Spacer(flex: 1),
-                                        //network
-                                        Image(
-                                            height: 100,
-                                            width: 100,
-                                            fit: BoxFit.contain,
-                                            repeat: ImageRepeat.noRepeat,
-                                            image: NetworkImage(
-                                                "https://cdn-icons-png.flaticon.com/128/3917/3917032.png"
+                            child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                children:  [
+                                    Draggable<Data>(
+                                        data: Data(message: "Hello Red", color: Colors.redAccent),
+                                        // SizedBox is a widget seams like container but it only has widget and height property
+                                        child: const SizedBox(
+                                            width: 50,
+                                            height: 50,
+                                            child: Material(
+                                                color: Colors.redAccent,
+                                                shape: StadiumBorder(),
+                                                elevation: 3,
                                             ),
                                         ),
-                                        // Spacer is a widget used to add space that fills remain space
-                                        Spacer(),
-                                    ],
-                                ),
-                            )
+                                        // SizedBox is a widget seams like container but it only has widget and height property
+                                        feedback: SizedBox(
+                                            width: 50,
+                                            height: 50,
+                                            child: Material(
+                                                color: Colors.redAccent.withOpacity(0.7),
+                                                shape: const StadiumBorder(),
+                                                elevation: 3,
+                                            ),
+                                        ),
+                                        // SizedBox is a widget seams like container but it only has widget and height property
+                                        childWhenDragging: const SizedBox(
+                                            width: 50,
+                                            height: 50,
+                                            child: null,
+                                        ),
+                                    ),
+                                    Draggable<Data>(
+                                        data: Data(message: "Hello Green", color: Colors.greenAccent),
+                                        // SizedBox is a widget seams like container but it only has widget and height property
+                                        child: const SizedBox(
+                                            width: 50,
+                                            height: 50,
+                                            child: Material(
+                                                color: Colors.greenAccent,
+                                                shape: StadiumBorder(),
+                                                elevation: 3,
+                                            ),
+                                        ),
+                                        // SizedBox is a widget seams like container but it only has widget and height property
+                                        feedback: SizedBox(
+                                            width: 50,
+                                            height: 50,
+                                            child: Material(
+                                                color: Colors.greenAccent.withOpacity(0.7),
+                                                shape: const StadiumBorder(),
+                                                elevation: 3,
+                                            ),
+                                        ),
+                                        // SizedBox is a widget seams like container but it only has widget and height property
+                                        childWhenDragging: const SizedBox(
+                                            width: 50,
+                                            height: 50,
+                                            child: null,
+                                        ),
+                                    )
+                                ],
+                            ),
                         ),
                         Flexible(
                             flex: 1,
-                            fit: FlexFit.tight,
-                            child: Container(
-                                color: Colors.red,
-                                child: Row(
-                                  children: const [
-                                        // Spacer is a widget used to add space that fills remain space
-                                        Spacer(),
-                                        // local
-                                        Image(
-                                            height: 100,
-                                            width: 100,
-                                            fit: BoxFit.contain,
-                                            repeat: ImageRepeat.noRepeat,
-                                            image: AssetImage("images/paper-plane.png"),
-                                        ),
-                                        // Spacer is a widget used to add space that fills remain space
-                                        Spacer(),
-                                  ],
-                                ),
+                            child: DragTarget(
+                                onWillAccept: (_) => true,
+                                onAccept: (data) => {
+                                    setState(() {
+                                        targetColor = (data as Data).color;
+                                    })
+                                },
+                                builder: (context, candidates, rejected) {
+                                    if (targetColor == null) {
+                                        return Container(
+                                            color: Colors.black12,
+                                        );
+                                    }
+                            
+                                    return Container(
+                                        color: targetColor,
+                                    );
+                                },
                             ),
                         )
-                    ],
+                    ]
                 ),
-            ),
-        ),
-    );
-  }
+            )
+        );
+    }
 }

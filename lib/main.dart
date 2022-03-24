fea8e974d7a34b00475e0bb1d2995855b65ca134
rizqyfahmi <rizqyfahmi@yahoo.com>
2022-03-25 05:39:11 +0700

    AppBar Custom Height

--- lib/main.dart
+++ lib/main.dart
@@ -10,98 +10,27 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: const MainPage(),
-      routes: {
-        MainPage.routeName: (context) => const MainPage(),
-        SecondPage.routeName: (context) => const SecondPage(),
-      },
-    );
-  }
-}
-
-class MainPage extends StatelessWidget {
-  const MainPage({ Key? key }) : super(key: key);
-  static const routeName = "/main";
-  
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Main Page"),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Hero(
-                tag: "hello",
-                child: GestureDetector(
-                  onTap: () {
-                    Navigator.of(context).pushNamed(SecondPage.routeName);
-                  },
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(50),
-                    child: const SizedBox(
-                      width: 100,
-                      height: 100,
-                      child: Image(
-                        image: NetworkImage(
-                            "https://humas.jatengprov.go.id/foto/1533008870602-Avatar%20logo%2068%20square.jpg"
-                          )
-                        ),
-                    ),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(200),
+          child: AppBar(
+            flexibleSpace: Align(
+              alignment: Alignment.bottomRight,
+              child: Container(
+                margin: const EdgeInsets.all(20),
+                child: const Text(
+                  "Appbar with custom height",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w700,
+                    color: Colors.white
                   ),
                 ),
-              )
-            ]
+              ) 
+            ),
           ),
-        ],
-      ),
-    );
-  }
-}
-
-class SecondPage extends StatelessWidget {
-  const SecondPage({ Key? key }) : super(key: key);
-  static const routeName = "/second";
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Second Page"),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Hero(
-                tag: "hello",
-                child: GestureDetector(
-                  onTap: () {
-                    Navigator.of(context).pop();
-                  },
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(50),
-                    child: const SizedBox(
-                      width: 100,
-                      height: 100,
-                      child: Image(
-                        image: NetworkImage(
-                          "https://humas.jatengprov.go.id/foto/1533008870602-Avatar%20logo%2068%20square.jpg"
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              )
-            ],
-          ),
-        ],
+        ),
       ),
     );
   }

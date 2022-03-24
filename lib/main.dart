4172109c523236fa7ee9e43b7514f48b3668e737
rizqyfahmi <rizqyfahmi@yahoo.com>
2022-03-24 07:07:07 +0700

    Card

--- lib/main.dart
+++ lib/main.dart
@@ -12,25 +12,33 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         home: Scaffold(
             appBar: AppBar(
-                title: const Text("AppBar Example"),
-                flexibleSpace: Container(
-                    decoration: const BoxDecoration(
-                        gradient: LinearGradient(
-                            colors: [
-                                Color(0xff0096ff),
-                                Color(0xff6610f2)
-                            ],
-                            begin: FractionalOffset.centerLeft,
-                            end: FractionalOffset.centerRight,
-                        ),
-                        image: DecorationImage(
-                            image: AssetImage("images/pattern.png"), 
-                            repeat: ImageRepeat.repeat
-                        )
-                    ),
-                ),
+                title: const Text("Hello world application"),
             ),
-        )
+            body: Column(
+                children: [
+                    buildCard(Icons.account_box, "Account Box"),
+                    buildCard(Icons.android, "Android")
+                ],
+            ),
+        ),
+    );
+  }
+
+  Card buildCard(IconData icon, String text) {
+    // Card is a widget used to create card like a panel in bootstrap
+    return Card(
+        color: Colors.white,
+        elevation: 5,
+        margin: const EdgeInsets.all(6),
+        child: Row(
+            children: [
+                Container(
+                    margin: const EdgeInsets.all(6),
+                    child: Icon(icon),
+                ),
+                Text(text)
+            ],
+        ),
     );
   }
 }

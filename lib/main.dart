0bf63ff13313898b1bbb6a25be37b4f087f03ce2
rizqyfahmi <rizqyfahmi@yahoo.com>
2022-03-24 09:43:34 +0700

    InkWell

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,48 +7,73 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({ Key? key }) : super(key: key);
 
-    @override
-    Widget build(BuildContext context) {
-        return const MaterialApp(
-            home: MainPage(),
-        );
-    }
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+        home: MainPage(),
+    );
+  }
 }
 
-class MainPage extends StatefulWidget {
-  const MainPage({
-    Key? key,
-  }) : super(key: key);
+class MainPage extends StatelessWidget {
+  const MainPage({ Key? key }) : super(key: key);
 
   @override
-  State<MainPage> createState() => _MainPageState();
-}
-
-class _MainPageState extends State<MainPage> {
-    Random random = Random();
-    @override
-    Widget build(BuildContext context) {
-        return Scaffold(
-            appBar: AppBar(
-                title: const Text("Media Query Screen Size"),
-            ),
-            body: (MediaQuery.of(context).orientation == Orientation.portrait) ? (
-                Column(
-                    children: List.generate(3, (index) => Container(
-                        width: 100,
-                        height: 100,
-                        color: Color.fromARGB(255, random.nextInt(256), random.nextInt(256), random.nextInt(256)),
-                    )),
-                )
-            ) : (
-                Row(
-                    children: List.generate(3, (index) => Container(
-                        width: 100,
-                        height: 100,
-                        color: Color.fromARGB(255, random.nextInt(256), random.nextInt(256), random.nextInt(256)),
-                    ))
-                )
-            ),
-        );
-    }
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            title: const Text("Hello world application"),
+        ),
+        body: Center(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                  ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          shape: const StadiumBorder()
+                      ),
+                      onPressed: (){},
+                      child: const Text("Eleveted Button")
+                  ),
+                  Material(
+                      borderRadius: BorderRadius.circular(20),
+                      elevation: 3,
+                        child: Container(
+                            width: 150,
+                            height: 40,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                gradient: const LinearGradient(
+                                    colors: [
+                                        Color.fromRGBO(0, 242, 96, 1),
+                                        Color.fromRGBO(5, 117, 230, 1),
+                                    ],
+                                    begin: Alignment.topLeft,
+                                    end: Alignment.bottomRight
+                                )
+                            ),
+                            child: Material(
+                                borderRadius: BorderRadius.circular(20),
+                                color: Colors.transparent,
+                                // InkWell is a widget used to bring ripple effect but it only work when it becomes child of Material widget
+                                child: InkWell(
+                                    borderRadius: BorderRadius.circular(20),
+                                    onTap: () {
+                                    
+                                    },
+                                    child: const Center(
+                                      child: Text(
+                                          "Custom Button",
+                                          style: TextStyle(color: Colors.white),
+                                      ),
+                                    ),
+                                ),
+                            ),
+                        ),
+                  )
+              ],
+          ),
+        ),
+    );
+  }
 }

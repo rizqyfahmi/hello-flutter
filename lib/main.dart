2d21fa22568faf1988b8a2c309581cfc6d6b2667
rizqyfahmi <rizqyfahmi@yahoo.com>
2022-03-25 14:16:28 +0700

    AudioPlayer

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,71 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({ Key? key }) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  AudioPlayer? audioPlayer;
+  String duration = "00:00:00";
+
+  _MyAppState() {
+    audioPlayer = AudioPlayer();  
+    audioPlayer?.onAudioPositionChanged.listen((drs) {
+      setState(() {
+        duration = drs.toString();
+      });
+    });
+    audioPlayer?.setReleaseMode(ReleaseMode.LOOP);
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text("Image Gradient"),
+          title: const Text("Music Player"),
         ),
         body: Center(
-          child: Container(
-            padding: const EdgeInsets.all(20),
-            child: ShaderMask(
-              shaderCallback: (bounds) {
-                return const LinearGradient(
-                  colors: [Colors.black, Colors.transparent],
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter
-                ).createShader(Rect.fromLTRB(0, 0, bounds.width, bounds.height));
-              },
-              blendMode: BlendMode.dstIn,
-              child: const Image(
-                image: NetworkImage("https://img.freepik.com/free-vector/beautiful-gradient-spring-landscape_23-2148448598.jpg?size=626&ext=jpg&ga=GA1.2.625627942.1648039534"),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                onPressed: () async {
+                  await audioPlayer?.play("https://luan.xyz/files/audio/ambient_c_motion.mp3");
+                }, 
+                child: const Text("Play")
               ),
-            ),
+              ElevatedButton(
+                onPressed: () async {
+                  await audioPlayer?.pause();
+                }, 
+                child: const Text("Pause")
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  await audioPlayer?.stop();
+                  setState(() {
+                    duration = "00:00:00";
+                  });
+                },
+                child: const Text("Stop")
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  await audioPlayer?.resume();
+                },
+                child: const Text("Resume")
+              ),
+              Text(duration)
+            ],
           ),
         ),
       ),

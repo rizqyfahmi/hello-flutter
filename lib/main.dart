944de4a6fa4494cb29a264baf92ba433309cb3f7
rizqyfahmi <rizqyfahmi@yahoo.com>
2022-03-25 10:43:47 +0700

    Gradient widget using ShaderMask

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,35 @@
 import 'package:flutter/material.dart';
-import 'package:hello_flutter/colorful_button.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text("Transform Application"),
+          title: const Text("Image Gradient"),
         ),
         body: Center(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: const [
-              ColorfulButton(
-                mainColor: Colors.pink,
-                secondColor: Colors.blue,
-                icon: Icons.adb
-              )
-            ],
+          child: Container(
+            padding: const EdgeInsets.all(20),
+            child: ShaderMask(
+              shaderCallback: (bounds) {
+                return const LinearGradient(
+                  colors: [Colors.black, Colors.transparent],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter
+                ).createShader(Rect.fromLTRB(0, 0, bounds.width, bounds.height));
+              },
+              blendMode: BlendMode.dstIn,
+              child: const Image(
+                image: NetworkImage("https://img.freepik.com/free-vector/beautiful-gradient-spring-landscape_23-2148448598.jpg?size=626&ext=jpg&ga=GA1.2.625627942.1648039534"),
+              ),
+            ),
           ),
         ),
       ),

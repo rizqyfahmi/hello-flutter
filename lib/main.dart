85d1291dd7ec7322e0e09760a3e3971b27cf6d88
rizqyfahmi <rizqyfahmi@yahoo.com>
2022-03-27 07:51:18 +0700

    Provider State Management

--- lib/main.dart
+++ lib/main.dart
@@ -1,69 +1,65 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:hello_flutter/application_color.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({ Key? key }) : super(key: key);
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  bool isActive = false;
-  TextEditingController controller = TextEditingController();
-
-  void setData() async {
-    SharedPreferences pref = await SharedPreferences.getInstance();
-    pref.setString("name", controller.text);
-    pref.setBool("isActive", isActive);
-  }
-
-  void getData() async {
-    SharedPreferences pref = await SharedPreferences.getInstance();
-    setState(() {
-      controller.text = pref.getString("name") ?? "";
-      isActive = pref.getBool("isActive") ?? false;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Shared Preferences App"),
+      // ChangeNotifierProvider is a widget used to provide an instance of ApplicationColor (state class)
+      home: ChangeNotifierProvider<ApplicationColor>(
+        create: (context) => ApplicationColor(),
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.black,
+            // Consumer is a widget used to watch/consume the changes that comes from ApplicationColor
+            title: Consumer<ApplicationColor>(
+              builder: (context, applicationColor, _) => Text(
+                "Provider State Management",
+                style: TextStyle(
+                  color: applicationColor.getColor()
+                ),
+              ),
+            ),
+          ),
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Consumer is a widget used to watch/consume the changes that comes from ApplicationColor
+              Consumer<ApplicationColor>(
+                builder: (context, applicationColor, _) => AnimatedContainer(
+                  duration: const Duration(seconds: 1),
+                  margin: const EdgeInsets.only(bottom: 10),
+                  height: 100,
+                  width: 100,
+                  color: applicationColor.getColor(),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text("AB"),
+                  // Consumer is a widget used to watch/consume the changes that comes from ApplicationColor
+                  Consumer<ApplicationColor>(
+                    builder: (context, applicationColor, _) => Switch(
+                      value: applicationColor.isLight,
+                      onChanged: (value) {
+                        applicationColor.isLight = value;
+                      }
+                    ),
+                  ),
+                  const Text("LB")
+                ],
+              )
+            ],
+          ),
         ),
-        body: Column(
-          children: [
-            TextField(
-              controller: controller,
-            ),
-            Switch(
-              value: isActive,
-              onChanged: (value) {
-                setState(() {
-                  isActive = value;
-                });
-              }
-            ),
-            ElevatedButton(
-              onPressed: () {
-                setData();
-              },
-              child: const Text("Save Data")
-            ),
-            ElevatedButton(
-              onPressed: () {
-                getData();
-              },
-              child: const Text("Load Data")
-            ),
-          ],
-        )
       ),
     );
   }

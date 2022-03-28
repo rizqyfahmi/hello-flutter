4ea661b9c24ed05626f595a97b978b0d4e4a46b9
rizqyfahmi <rizqyfahmi@yahoo.com>
2022-03-28 16:54:14 +0700

    Timer using Provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,159 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:hello_flutter/profile.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+
   const MyApp({ Key? key }) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Timer? timer;
+  String textButton = "Start";
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Doc Comments"),
-        ),
-        body: const Center(
-          child: Profile(),
+      home: ChangeNotifierProvider<TimeState>(
+        create: (context) => TimeState(),
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text("Custom Progress Bar"),
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Consumer<TimeState>(
+                  builder: (context, timeState, _) => CustomProgressBar(
+                    width: 150,
+                    currentValue: timeState.time,
+                    totalValue: 100,
+                  ),
+                ),
+                Consumer<TimeState>(
+                  builder: (context, timeState, _) => Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          if (timer != null) {
+                            timer?.cancel();
+                            timer = null;
+                            setState(() {
+                              textButton = "Start";
+                            });
+                            return;
+                          }
+
+                          timer = Timer.periodic(const Duration(seconds: 1), (tm) {
+                            if (timeState.time > 0) {
+                              timeState.time -= 10;
+                              return;
+                            }
+
+                            tm.cancel();
+                            timer = null;
+                            setState(() {
+                              textButton = "Start";
+                            });
+                          });
+
+                          setState(() {
+                            textButton = "Stop";
+                          });
+                        },
+                        child: Text(textButton)
+                      ),
+                      const SizedBox(width: 10),
+                      ElevatedButton(
+                        onPressed: () {
+                          timeState.time = 100;
+                        },
+                        child: const Text("Reset")
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
+  }
+}
+
+class CustomProgressBar extends StatelessWidget {
+  final double? width;
+  final double currentValue;
+  final double totalValue;
+
+  const CustomProgressBar({ Key? key, this.width, required this.currentValue, required this.totalValue }) : super(key: key);
+
+  Color getProgressColor(double progress) {
+    if (progress < 30) {
+      return Colors.red;
+    }
+
+    if (progress < 50) {
+      return Colors.yellow;
+    }
+
+    return Colors.green;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Icon(Icons.timer),
+        const SizedBox(width: 10),
+        Stack(
+          children: [
+            Container(
+              width: width,
+              height: 10,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(5),
+                color: Colors.grey[300]
+              ),
+            ),
+            Material(
+              elevation: 5,
+              borderRadius: BorderRadius.circular(5),
+              child: AnimatedContainer(
+                duration: const Duration(milliseconds: 500),
+                width: (currentValue / totalValue) * (width ?? 1),
+                height: 10,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5),
+                  color: getProgressColor((currentValue / totalValue) * 100)
+                ),
+              ),
+            )
+          ],
+        )
+      ],
+    );
+  }
+}
+
+class TimeState with ChangeNotifier {
+  double _time = 100;
+
+  double get time => _time;
+  set time(double value) {
+    _time = value;
+    notifyListeners();
   }
 }

48d0dcf9d123ee752df7da524bdb782f411f5043
rizqyfahmi <rizqyfahmi@yahoo.com>
2022-03-29 10:17:11 +0700

    SelectableText, ToggleButtons, and ColorFiltered

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,59 @@
 import 'package:flutter/material.dart';
-import 'package:hello_flutter/product_card.dart';
-import 'package:hello_flutter/product_state.dart';
-import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({ Key? key }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: MainPage(),
-    );
-  }
+  State<MyApp> createState() => _MyAppState();
 }
 
-class MainPage extends StatelessWidget {
-  const MainPage({ Key? key }) : super(key: key);
+class _MyAppState extends State<MyApp> {
+  List<bool> isSelected = [false, true, false];
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => ProductState(quantity: 0),
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text("Hello world application"),
-        ),
-        body: Center(
-          child: Consumer<ProductState>(
-            builder: (context, productState, _) => ProductCard(
-              title: "Buah-buahan",
-              description: "Buah-buahan",
-              quantity: productState.quantity,
-              onIncreament: () {
-                productState.quantity++;
-              },
-              onDecrement: () {
-                if (productState.quantity <= 0) return;
-                productState.quantity--;
-              },
+    return MaterialApp(
+      home: ColorFiltered(
+        colorFilter: const ColorFilter.mode(Colors.green, BlendMode.color),
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text("Hello world application"),
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // SelectableText is a widget used to display text but it can be selectable using finger
+                const SelectableText(
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+                  showCursor: true,
+                ),
+                // It is like group button in bootstrap
+                ToggleButtons(
+                  children: const [
+                    Icon(Icons.adb),
+                    Icon(Icons.ac_unit_sharp),
+                    Icon(Icons.account_balance_wallet_sharp),
+                  ], 
+                  isSelected: isSelected,
+                  onPressed: (index) {
+                    setState(() {
+                      for (var i = 0; i < isSelected.length; i++) {
+                        if (index == i) {
+                          isSelected[index] = !isSelected[index];
+                          continue;
+                        }
+      
+                        isSelected[i] = false;
+                      }
+                    });
+                  },
+                )
+              ],
             ),
           ),
         ),

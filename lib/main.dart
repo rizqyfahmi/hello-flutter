5fee994aab145100f169bfad91130051b849a364
rizqyfahmi <rizqyfahmi@yahoo.com>
2022-03-29 21:56:42 +0700

    Hive Database

--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,110 @@
-import 'package:flutter/material.dart';
+import 'dart:developer';
 
-void main() {
+import 'package:flutter/material.dart';
+import 'package:hello_flutter/model/monster.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:path_provider/path_provider.dart' as path;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final appDocumentDirectory = await path.getApplicationDocumentsDirectory();
+  Hive.initFlutter(appDocumentDirectory.path);
+  Hive.registerAdapter(MonsterAdapter());
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
-
+class MyApp extends StatelessWidget {
   const MyApp({ Key? key }) : super(key: key);
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MainPage(),
+    );
+  }
 }
 
-class _MyAppState extends State<MyApp> {
-  final List<String> names = ["John", "Siera", "Monica"];
-  String? selectedName;
-
-  List<DropdownMenuItem<String>> buildDropdownMenuItem(List<String> names) {
-    return names.map((String name) => (
-      DropdownMenuItem<String>(
-        child: Text(name),
-        value: name,
-      )
-    )).toList();
-  }
+class MainPage extends StatelessWidget {
+  const MainPage({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("DropdownButton Demo"),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              DropdownButton<String>(
-                items: buildDropdownMenuItem(names),
-                value: selectedName,
-                onChanged: (String? value) {
-                  setState(() {
-                    selectedName = value;
-                  });
-                }
-              ),
-              Text(selectedName ?? "Belum ada yang terpilih")
-            ],
-          ),
-        ),
-      )
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Hive database demo"),
+      ),
+      body: FutureBuilder(
+        future: Hive.openBox("monsterBox"),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState != ConnectionState.done) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          if (snapshot.hasError) {
+            return Center(
+              child: Text(snapshot.error.toString()),
+            );
+          }
+
+          final monsterBox = Hive.box("monsterBox");
+          if (monsterBox.length == 0){
+            monsterBox.add(Monster("Monster 1", 1));
+            monsterBox.add(Monster("Monster 2", 2));
+          }
+
+          return ValueListenableBuilder(
+            valueListenable: monsterBox.listenable(),
+            builder: (context, Box box, _) {
+              return ListView.builder(
+                itemCount: monsterBox.length,
+                itemBuilder: (context, index) {
+                  Monster monster = monsterBox.getAt(index);
+                  return Container(
+                    margin: const EdgeInsets.all(15),
+                    padding: const EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [BoxShadow(
+                        color: Colors.black.withOpacity(0.2),
+                        blurRadius: 6,
+                        offset: const Offset(3, 3)
+                      )]
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "${monster.name} - ${monster.level}"
+                        ),
+                        Row(
+                          children: [
+                            IconButton(
+                              onPressed: () {
+                                box.putAt(index, Monster(monster.name, monster.level + 1));
+                              }, 
+                              icon: const Icon(Icons.move_up)
+                            ),
+                            IconButton(
+                              onPressed: () {
+                                box.deleteAt(index);
+                              }, 
+                              icon: const Icon(Icons.delete)
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  );
+                },
+              );
+            }
+          );
+        },
+      ),
     );
   }
 }

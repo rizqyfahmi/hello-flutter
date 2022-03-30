5b299f80e563d71cbfa59261418ef6cc92826a46
rizqyfahmi <rizqyfahmi@yahoo.com>
2022-03-30 17:29:46 +0700

    PageView Scale Transition

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:developer' as developer;
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() async {
@@ -25,12 +28,24 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
-  PageController controller = PageController(initialPage: 1, viewportFraction: 0.6);
+  double currentPage = 0;
+  PageController controller = PageController(initialPage: 0, viewportFraction: 0.6);
   List<String> images = [
     "https://s1.bukalapak.com/img/68286857232/large/Poster_Film___Avengers_Endgame___Marvel_Studios___Movie_Post.jpg",
     "https://www.washingtonpost.com/graphics/2019/entertainment/oscar-nominees-movie-poster-design/img/black-panther-web.jpg",
     "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/5f51f696241415.5eb2d19f8b3e1.png"
   ];
+
+  @override
+  void initState() {
+    super.initState();
+    controller.addListener(() {
+      setState(() {
+        currentPage = controller.page!;
+      });
+    });
+    
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -43,10 +58,12 @@ class _MainPageState extends State<MainPage> {
           controller: controller,
           itemCount: images.length,
           itemBuilder: (context, index) {
+            double difference = min(1, (index - currentPage).abs());
+
             return Center(
               child: Container(
-                height: 300,
-                width: 200,
+                height: 300 * (1 - (0.3 * difference)),
+                width: 200 * (1 - (0.3 * difference)),
                 margin: const EdgeInsets.all(10),
                 decoration: BoxDecoration(
                   color: Colors.red,

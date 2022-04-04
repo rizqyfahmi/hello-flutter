ed346ce3f3dcbec9343d4b9778fd5a15aa54a4eb
rizqyfahmi <rizqyfahmi@yahoo.com>
2022-04-04 09:34:51 +0700

    GetX Dialog

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -33,6 +33,48 @@ class LoginPage extends StatelessWidget {
                 );
               },
               child: const Text("Open SnackBar")
+            ),
+            const SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(onPressed: () {
+                    Get.defaultDialog(
+                      title: "Default Dialog",
+                      content: const Text("Hello from default dialog"),
+                      textCancel: "Close",
+                      textConfirm: "Save",
+                      confirmTextColor: Colors.white,
+                      onConfirm: () {
+                        Get.back();
+                      }
+                    );
+                  }, 
+                  child: const Text("Default Dialog")
+                ),
+                const SizedBox(width: 10),
+                ElevatedButton(onPressed: () {
+                    Get.dialog(
+                      const AlertDialog(
+                        content: Text("Hello Dialog"),
+                      )
+                    );
+                  }, 
+                  child: const Text("Dialog")
+                ),
+                const SizedBox(width: 10),
+                ElevatedButton(onPressed: () {
+                    Get.generalDialog(
+                      barrierLabel: "Close",
+                      barrierDismissible: true,
+                      pageBuilder: (_, __, ___) => const AlertDialog(
+                        content: Text("Hello Dialog"),
+                      )
+                    );
+                  }, 
+                  child: const Text("General Dialog")
+                )
+              ],
             )
           ],
         ),

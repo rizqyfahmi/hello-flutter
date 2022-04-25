b57feed1934569b07a3b4af038ad18d77987ea6a
rizqyfahmi <rizqyfahmi@yahoo.com>
2022-04-25 19:39:23 +0700

    Create hole frame using ColorFiltered

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,6 @@
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:location/location.dart';
 
 void main() async {
   runApp(const MyApp());
@@ -20,78 +18,55 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MainPage extends StatefulWidget {
+class MainPage extends StatelessWidget {
   const MainPage({ Key? key }) : super(key: key);
-
-  @override
-  State<MainPage> createState() => _MainPageState();
-}
-
-class _MainPageState extends State<MainPage> {
-  late GoogleMapController _controller;
-  final Location _location = Location();
-
-  static const CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  Future<void> _goToTheLake() async {
-    _controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
-  }
-
-  void getLocation() async {
-    bool _serviceEnabled = await _location.serviceEnabled();
-    if (!_serviceEnabled) {
-      _serviceEnabled = await _location.requestService();
-      if (!_serviceEnabled) {
-        return;
-      }
-    }
-
-    PermissionStatus _permissionGranted = await _location.hasPermission();
-    if (_permissionGranted == PermissionStatus.denied) {
-      _permissionGranted = await _location.requestPermission();
-      if (_permissionGranted != PermissionStatus.granted) {
-        return;
-      }
-    }
-
-    LocationData locationData = await _location.getLocation();
-    _controller.animateCamera(CameraUpdate.newCameraPosition(
-      CameraPosition(target: LatLng(locationData.latitude ?? 0.0, locationData.longitude ?? 0.0), zoom: 15)
-    ));
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Google Map"),
+        title: const Text("Color Filtered"),
       ),
-      body: GoogleMap(
-        initialCameraPosition: const CameraPosition(target: LatLng(-6.121435, 106.774124), zoom: 14),
-        myLocationButtonEnabled: false,
-        zoomControlsEnabled: false,
-        onMapCreated: (controller) {
-          _controller = controller;
-          // _location.onLocationChanged.listen((event) {
-          //   print("listen: ${event.latitude ?? 0.0}, ${event.longitude ?? 0.0}");
-          // });
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.pin_drop),
-        onPressed: () {
-          getLocation();  
-          // _goToTheLake();
-        }
+      body: Stack(
+        children: [
+          Image.network(
+            "https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80",
+            fit: BoxFit.cover,
+            height: double.infinity,
+          ),
+          ColorFiltered(
+            colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.8), BlendMode.srcOut),
+            child: Stack(
+              children: [
+                Container(
+                  width: double.infinity,
+                  height: double.infinity,
+                  decoration: const BoxDecoration(
+                    color: Colors.green,
+                    backgroundBlendMode: BlendMode.dstOut
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.center,
+                  child: Padding(
+                    padding: EdgeInsets.only(left: 16, right: 16),
+                    child: AspectRatio(
+                      aspectRatio: 1/1,
+                      child: Container(
+                        height: MediaQuery.of(context).size.width,
+                        width: 100,
+                        decoration: BoxDecoration(
+                          color: Colors.red,
+                          borderRadius: BorderRadius.circular(200)
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }

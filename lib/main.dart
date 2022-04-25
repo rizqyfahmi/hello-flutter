63c249307e9c2abeb07ea069af4d4cc3a33a5e12
rizqyfahmi <rizqyfahmi@yahoo.com>
2022-04-25 10:08:50 +0700

    Integrating google map

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,13 @@
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:hello_flutter/LoadingProcessor.dart';
-
-/*
-  - A StreamProvider has identical behavior to FutureProvider but instead of creating a Future, the provider creates a Stream
-    - It is typically used for:
-      - Listening to Firebase or web-sockets
-      - Rebuilding another provider every few seconds
-  - "autoDispose" used to destroy the state of a provider when it is no-longer used.
-    - There are multiple reasons for doing so, such as:
-      - When using Firebase, to close the connection and avoid unnecessary cost.
-      - To reset the state when the user leaves a screen and re-enters it.
-  - "family" used for passing parameter
- */
-final loadingProvider = StreamProvider.autoDispose.family<int, int>((ref, multiple) async* {
-  // init LoadingProcessor
-  final loadingProcessor = LoadingProcessor(multiple: multiple);
-  // With autoDispose modifier gives us access to the onDispose call on ref and allows us to destroy the state of a provider when it is no longer used
-  ref.onDispose(() => loadingProcessor.controller.sink.close());
-  // We listen to values that are emitted from loadingProcessor.stream in a for loop and then perform the actions needed
-  await for (final value in loadingProcessor.stream) {
-    if (value == 100) {
-      loadingProcessor.controller.sink.close();
-    }
-    // We then yield the value that the stream provides emits
-    yield value;
-  }
-});
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 void main() async {
-  // For widgets to be able to read providers, we need to wrap the entire application in a "ProviderScope" widget.
-  // This is where the state of our providers will be stored.
-  runApp(const ProviderScope(child: MyApp()));
+  if (defaultTargetPlatform == TargetPlatform.android) {
+    AndroidGoogleMapsFlutter.useAndroidViewSurface = true;
+  }
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -41,39 +16,54 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: HomePage(),
+      home: MainPage(),
     );
   }
 }
 
-class HomePage extends ConsumerWidget {
-  const HomePage({ Key? key }) : super(key: key);
+class MainPage extends StatefulWidget {
+  const MainPage({ Key? key }) : super(key: key);
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    AsyncValue<int> loading = ref.watch(loadingProvider(5));
-    
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  late GoogleMapController _controller;
+
+  static const CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  static const CameraPosition _kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(37.43296265331129, -122.08832357078792),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
+
+  Future<void> _goToTheLake() async {
+    _controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Riverpod's StreamProvider"),
+        title: const Text("Google Map"),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            loading.when(
-              data: (percent) {
-                return Text("loading is $percent");
-              }, 
-              error: (err, stack) {
-                return Text('Error: $err');
-              }, 
-              loading: () {
-                return const CircularProgressIndicator();
-            }),
-          ],
-        ),
-      )
+      body: GoogleMap(
+        initialCameraPosition: _kGooglePlex,
+        myLocationButtonEnabled: false,
+        zoomControlsEnabled: false,
+        onMapCreated: (controller) {
+          _controller = controller;
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.pin_drop),
+        onPressed: _goToTheLake
+      ),
     );
   }
 }

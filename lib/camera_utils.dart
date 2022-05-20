486f4101d7da63b496adfbf4bcf6c14fda3bad6e
rizqyfahmi <rizqyfahmi@yahoo.com>
2022-05-20 13:48:36 +0700

    Splitting getInputImage

--- lib/camera_utils.dart
+++ lib/camera_utils.dart
@@ -14,20 +14,17 @@ class CameraUtils {
     });
   }
 
-  static Future<List<Barcode>> detectBarcode(
-      {required CameraImage image,
-      required InputImageRotation imageRotation,
-      required BarcodeScanner barcodeScanner}) async {
-    
-    // final InputImage inputImage = getInputImage(image, camera);
-    // print("Hello: ${inputImage.toString()}");
-    
-    
-    
+  static Future<List<Barcode>> detectBarcode({
+    required CameraImage image,
+    required InputImageRotation imageRotation,
+    required BarcodeScanner barcodeScanner
+  }) async {
+    return barcodeScanner.processImage(
+      getInputImage(image, imageRotation)
+    );
+  }
 
-    // return barcodeScanner.processImage(inputImage);
-
-
+  static InputImage getInputImage(CameraImage image, InputImageRotation imageRotation) {
     final WriteBuffer allBytes = WriteBuffer();
     for (final Plane plane in image.planes) {
       allBytes.putUint8List(plane.bytes);
@@ -45,37 +42,6 @@ class CameraUtils {
         );
       },
     ).toList();
-
-    final inputImageData = InputImageData(
-      size: imageSize,
-      imageRotation: imageRotation,
-      inputImageFormat: inputImageFormat,
-      planeData: planeData,
-    );
-
-    return barcodeScanner.processImage(
-      InputImage.fromBytes(bytes: bytes, inputImageData: inputImageData),
-    );
-  }
-
-  static InputImage getInputImage(CameraImage image, CameraController camera) {
-    final WriteBuffer allBytes = WriteBuffer();
-    for (Plane plane in image.planes) {
-      allBytes.putUint8List(plane.bytes);
-    }
-
-    final bytes = allBytes.done().buffer.asUint8List();
-
-    final Size imageSize = Size(image.width.toDouble(), image.height.toDouble());
-    final InputImageRotation imageRotation = InputImageRotationValue.fromRawValue(camera.description.sensorOrientation) ?? InputImageRotation.rotation0deg;
-    final InputImageFormat inputImageFormat = InputImageFormatValue.fromRawValue(image.format.raw) ?? InputImageFormat.nv21;
-
-    final planeData = image.planes.map((Plane plane) {
-      return InputImagePlaneMetadata(
-          bytesPerRow: plane.bytesPerRow,
-          height: plane.height,
-          width: plane.width);
-    }).toList();
 
     final inputImageData = InputImageData(
       size: imageSize,

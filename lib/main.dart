3a55527db04290a0089aca8de69e0d4adfa1b888
rizqyfahmi <rizqyfahmi@yahoo.com>
2022-05-25 18:55:58 +0700

    Basic showcase/walkthrough/intro/guide

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 
 import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -21,13 +20,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      // theme: ThemeData(
-      //   appBarTheme: const AppBarTheme(
-      //     systemOverlayStyle: SystemUiOverlayStyle(
-      //       statusBarColor: Colors.red
-      //     )
-      //   )
-      // ),
       home: MainPage()
     );
   }
@@ -45,10 +37,29 @@ class _MainPageState extends State<MainPage> {
   ScrollController yellowScrollController = ScrollController();
   ScrollController redScrollController = ScrollController();
 
+  GlobalKey childKey = GlobalKey();
+  GlobalKey parentKey = GlobalKey();
+
+  RenderBox? box;
+
+  void afterLayout() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      final RenderBox? childBox = childKey.currentContext?.findRenderObject() as RenderBox;
+      
+      if (childBox == null) return;
+
+      setState(() {
+        box = childBox;
+      });
+    });
+  }
+
   @override
   void initState() {
     super.initState();
-
+    
+    afterLayout();
+    
     yellowScrollController.addListener(() {
       print("Hello Yellow: ${yellowScrollController.offset}");
       greenScrollController.jumpTo(yellowScrollController.offset * 0.25); // greenScrollController more slower than yellowScrollController
@@ -63,66 +74,51 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
+    Offset? offset = box?.localToGlobal(Offset.zero);
 
     return Scaffold(
       body: Stack(
         children: [
-          Container(
-            color: Colors.red,
-            height: size.height,
-            width: size.width
-          ),
-          SafeArea(
-            child: Stack(
-              children: [
-                SingleChildScrollView(
-                  controller: greenScrollController,
-                  child: Container(
-                    padding: const EdgeInsets.only(top: 18),
-                    width: size.width,
-                    height: size.height + 100,
-                    color: Colors.green,
-                    child: const Text(
-                      "Green",
-                      style: TextStyle(
-                        fontSize: 18
-                      ),
-                    ),
-                  ),
-                ),
-                SingleChildScrollView(
-                  controller: yellowScrollController,
-                  child: Container(
-                    margin: const EdgeInsets.only(top: 100),
-                    width: size.width,
-                    height: size.height + 100,
-                    color: Colors.yellow,
-                    child: const Text(
-                      "Yellow",
-                      style: TextStyle(
-                        fontSize: 18
-                      ),
-                    ),
-                  ),
-                ),
-                SingleChildScrollView(
-                  controller: redScrollController,
-                  child: Container(
-                    margin: const EdgeInsets.only(top: 150),
-                    width: size.width,
-                    height: size.height + 150,
-                    color: Colors.red,
-                    child: const Text(
-                      "Red",
-                      style: TextStyle(
-                        fontSize: 18
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
+         Container(
+           width: size.width,
+           height: size.height + 150,
+           color: Colors.red,
+           child: Column(
+             children: [
+               const Center(
+                 child: Text(
+                   "Hello one time",
+                   style: TextStyle(
+                     fontSize: 28
+                   ),
+                 ),
+               ),
+               const SizedBox(height: 50),
+               Container(
+                 key: childKey,
+                 color: Colors.white,
+                 padding: const EdgeInsets.all(8),
+                 height: 100,
+                 width: 200,
+               )
+             ]
+           ),
+         ),
+         Positioned(
+           top: offset?.dy ?? 0,
+           left: offset?.dx ?? 0,
+           child: Container(
+             color: Colors.red,
+             width: box?.size.width ?? 0,
+             height: box?.size.height ?? 0,
+           )
+         ),
+         Expanded(
+           child: CustomPaint(
+             painter: ShowcasePainter(renderBox: box),
+             child: Container(),
+           ),
+         )
         ],
       ),
     );
@@ -155,6 +151,46 @@ class CircularChartPainter extends CustomPainter {
     paint.color = Colors.green;
     canvas.drawArc(circle, startAngle, endAngle, false, paint);
 
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
+
+}
+
+class ShowcasePainter extends CustomPainter {
+  final RenderBox? renderBox;
+
+  ShowcasePainter({
+    required this.renderBox
+  });
+
+  @override
+  void paint(Canvas canvas, Size size) {
+
+    if (renderBox == null) return;
+
+    final Size sizeBox = renderBox!.size;
+    final Offset offsetBox = renderBox!.localToGlobal(Offset.zero);
+
+    print("Hello: $offsetBox $sizeBox");
+
+    final Path path = Path.combine(
+      PathOperation.difference, 
+      Path()..addRect(Rect.fromLTWH(0, 0, size.width, size.height))..close(), 
+      Path()..addRRect(
+        RRect.fromRectAndRadius(
+          // Rect.fromLTRB(offsetBox.dx, offsetBox.dy - sizeBox.height, offsetBox.dx + sizeBox.width, offsetBox.dy), 
+          Rect.fromLTWH(offsetBox.dx, offsetBox.dy, sizeBox.width, sizeBox.height), 
+          const Radius.circular(0)
+        )
+      )
+    )..close();
+
+    final Paint paint = Paint();
+    paint.color = Colors.black54;
+
+    canvas.drawPath(path, paint);
   }
 
   @override

fd6ea9b5901c096a964336a9b2428222a8063155
rizqyfahmi <rizqyfahmi@yahoo.com>
2022-05-25 22:28:31 +0700

    Showcase/walkthrough/intro/guide multiple widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,10 @@
 
 import 'dart:math';
-import 'dart:ui';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 void main() async {
-
-  SystemChrome.setSystemUIOverlayStyle(
-    const SystemUiOverlayStyle(
-      statusBarColor: Colors.red
-    )
-  );
+  
+  
   runApp(const MyApp());
 }
 
@@ -38,83 +32,172 @@ class _MainPageState extends State<MainPage> {
   ScrollController yellowScrollController = ScrollController();
   ScrollController redScrollController = ScrollController();
 
-  GlobalKey childKey = GlobalKey();
-  GlobalKey parentKey = GlobalKey();
-
+  List<GlobalKey> globalKeys = [
+    GlobalKey(), GlobalKey(), GlobalKey()
+  ];
+  
   RenderBox? box;
+  int currentIndex = 0;
+  bool isFinished = false;
 
   void afterLayout() {
     WidgetsBinding.instance.addPostFrameCallback((_) {
-      final RenderBox? childBox = childKey.currentContext?.findRenderObject() as RenderBox;
-      
-      if (childBox == null) return;
+      onChangeLayout();
+    });
+  }
 
+  void onChangeLayout() {
+    if (currentIndex == globalKeys.length) {
       setState(() {
-        box = childBox;
+        isFinished = !isFinished;
       });
+      return;
+    }
+
+    final RenderBox? currentBox = globalKeys[currentIndex].currentContext?.findRenderObject() as RenderBox;
+    final int nextIndex = currentIndex + 1;
+
+    if (currentBox == null) return;
+
+    setState(() {
+      box = currentBox;
+      currentIndex = nextIndex;
     });
   }
 
   @override
   void initState() {
     super.initState();
-    
     afterLayout();
-    
-    yellowScrollController.addListener(() {
-      print("Hello Yellow: ${yellowScrollController.offset}");
-      greenScrollController.jumpTo(yellowScrollController.offset * 0.25); // greenScrollController more slower than yellowScrollController
-    });
-
-    redScrollController.addListener(() {
-      print("Hello Red: ${redScrollController.offset}");
-      yellowScrollController.jumpTo(redScrollController.offset * 0.65); // yellowScrollController more slower than redScrollController
-    });
   }
 
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    final padding = MediaQuery.of(context).padding;
+    final Size size = MediaQuery.of(context).size;
+    final EdgeInsets padding = MediaQuery.of(context).padding;
+    final Offset? offset = box?.localToGlobal(Offset.zero);
     
     return Scaffold(
-      body: SafeArea(
-        child: Stack(
-          children: [
-            Container(
-              width: size.width,
-              height: size.height + 150,
-              color: Colors.red,
-              child: Column(
-                children: [
-                  const SizedBox(height: 50),
-                  Container(
-                    color: Colors.red,
-                    padding: const EdgeInsets.all(8),
-                    child: Text(
-                      "Hello one time",
-                      key: childKey,
-                      style: const TextStyle(
-                        fontSize: 28
-                      ),
-                    ),
-                  )
-                ]
-              ),
-            ),
-            Column(
+      body: Stack(
+        children: [
+          Column(
+            children: [
+              Expanded(
+                child: Container(
+                  color: Colors.red,
+                )
+              )
+            ],
+          ),
+          SafeArea(
+            child: Stack(
               children: [
-                Expanded(
-                  child: CustomPaint(
-                    painter: ShowcasePainter(renderBox: box, top: padding.top),
-                    child: Container(),
+                Container(
+                  width: size.width,
+                  height: size.height + 150,
+                  color: Colors.red,
+                  child: Column(
+                    children: [
+                      const SizedBox(height: 20),
+                      Container(
+                        color: Colors.red,
+                        padding: const EdgeInsets.all(8),
+                        child: Text(
+                          "Hello one time",
+                          key: globalKeys[0],
+                          style: const TextStyle(
+                            fontSize: 28
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 50),
+                      Container(
+                        color: Colors.red,
+                        padding: const EdgeInsets.all(8),
+                        child: Text(
+                          "Hello second time",
+                          key: globalKeys[1],
+                          style: const TextStyle(
+                            fontSize: 28
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 100),
+                      Container(
+                        color: Colors.red,
+                        padding: const EdgeInsets.all(8),
+                        child: Text(
+                          "Hola",
+                          key: globalKeys[2],
+                          style: const TextStyle(
+                            fontSize: 28
+                          ),
+                        ),
+                      )
+                    ]
                   ),
                 ),
               ],
-            )
-          ],
-        ),
+            ),
+          ),
+          renderOverlay(padding: padding),
+          renderButton(offset: offset, padding: padding)
+        ],
       ),
+    );
+  }
+
+  Widget renderOverlay({required EdgeInsets padding}) {
+    if (isFinished) {
+      return Container();
+    }
+
+    return Column(
+      children: [
+        Expanded(
+          child: CustomPaint(
+            painter: ShowcasePainter(renderBox: box, top: padding.top),
+            child: Container(),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget renderButton({required Offset? offset, required EdgeInsets padding}) {
+
+    String text = "Selanjutnya";
+
+    if (currentIndex >= globalKeys.length) { // currentIndex is gonna be 1 after call "afterLayout" at initState
+      text = "Selesai";
+    }
+
+    if (isFinished) {
+      return Container();
+    }
+
+    return Positioned(
+      top: (offset?.dy ?? 0) + (box?.size.height ?? 0) + 16,
+      left: offset?.dx ?? 0,
+      child: Container(
+        padding: const EdgeInsets.all(4),
+        decoration: BoxDecoration(
+          border: Border.all(width: 2, color: Colors.white),
+          borderRadius: BorderRadius.circular(16)
+        ),
+        child: TextButton(
+          onPressed: () {
+            onChangeLayout();
+          },
+          child: Text(
+            text,
+            style: const TextStyle(
+              fontSize: 16,
+              color: Colors.white
+            )
+          )
+        ),
+      )
     );
   }
 }
@@ -168,17 +251,14 @@ class ShowcasePainter extends CustomPainter {
 
     final Size sizeBox = renderBox!.size;
     final Offset offsetBox = renderBox!.localToGlobal(Offset.zero);
-
-    print("Hello: $offsetBox $sizeBox ${top}");
-
     final Path path = Path.combine(
       PathOperation.difference, 
       Path()..addRect(Rect.fromLTWH(0, 0, size.width, size.height))..close(), 
       Path()..addRRect(
         RRect.fromRectAndRadius(
           // Rect.fromLTRB(offsetBox.dx, offsetBox.dy - sizeBox.height, offsetBox.dx + sizeBox.width, offsetBox.dy), 
-          Rect.fromLTWH(offsetBox.dx, offsetBox.dy - top, sizeBox.width, sizeBox.height), 
-          const Radius.circular(0)
+          Rect.fromLTWH(offsetBox.dx, offsetBox.dy, sizeBox.width, sizeBox.height), 
+          const Radius.circular(8)
         )
       )
     )..close();
